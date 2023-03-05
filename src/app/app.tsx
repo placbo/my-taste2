f9d36916fd7646042c1ccb8b7ba8a9b9feb0ab31
PCB <perbjester@gmail.com>
2023-03-05 21:57:40 +0100

    first commit

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,5 @@
 import styled from '@emotion/styled';
 
-import NxWelcome from './nx-welcome';
-
 const StyledApp = styled.div`
   // Your style here
 `;
@@ -9,7 +7,7 @@ const StyledApp = styled.div`
 export function App() {
   return (
     <StyledApp>
-      <NxWelcome title="my-taste2" />
+      <h1>PCB</h1>
     </StyledApp>
   );
 }
